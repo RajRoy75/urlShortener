34650c3e3a1e07048af1871d44906fcfe7b2064c
Raj Roy <rajroy894290@gmail.com>
2025-11-28 12:48:04 +0530

    backend ready to deploy

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { pool } from "./db.js";
+import cors from 'cors';
 import linksRouter from "./routes/links.js";
 import healthRouter from "./routes/health.js";
 
@@ -7,10 +7,11 @@ const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/", healthRouter);
 app.use("/api", linksRouter);
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
 });
